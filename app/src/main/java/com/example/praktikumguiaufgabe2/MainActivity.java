de01d121e8fef7e4815579ce7efd89315571ff09
kasarchu <49198055+kasarchu@users.noreply.github.com>
2019-12-09 17:15:44 +0100

    layout toolbar

--- app/src/main/java/com/example/praktikumguiaufgabe2/MainActivity.java
+++ app/src/main/java/com/example/praktikumguiaufgabe2/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.praktikumguiaufgabe2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.quit: tv.setText("Beenden");
+            break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
