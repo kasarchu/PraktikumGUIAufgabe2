71f54945295313ce7099221e71ca6d7183e56099
kasarchu <49198055+kasarchu@users.noreply.github.com>
2019-12-16 16:39:20 +0100

    ListView implementiert ImageView implementiert

--- app/src/main/java/com/example/praktikumguiaufgabe2/MainActivity.java
+++ app/src/main/java/com/example/praktikumguiaufgabe2/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.ListViewCompat;
 
 import android.app.ListActivity;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,30 +21,40 @@ import android.widget.TextView;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-//public class MainActivity extends AppCompatActivity {
-public class MainActivity extends ListActivity {
+// AppCompatActivity wegen Toolbar
+public class MainActivity extends AppCompatActivity {
 
-// ohne AppCompatActivity fehlt ToolBar
-// ohne ListActivity fehlt ListView...this.setListAdapter !!
+    private Button btnAuswählen;
     private TextView tv;
     private ListView lv;
+    private ImageView iv;
 
-    private String [] content = {
-            "Fotos", "Bildschirmfotos", "Anderes Verzeichnis..."};
+    // wird dem Adapter übergeben
+    private String [] content = {"Fotos", "Bildschirmfotos", "Anderes Verzeichnis..."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView) findViewById(android.R.id.list);
+        btnAuswählen = (Button) findViewById(R.id.btnAuswählen);
+
+        // ListView initialisieren
+        lv = (ListView) findViewById(R.id.listView);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
+        // Adapter anlegen um die ListView zu befüllen
         ArrayAdapter<String> myList = new ArrayAdapter<String>(this,
                                     R.layout.simple_list_item_multiple_choice, content);
 
-        // CheckedTextView erstellen und verknüpfen!!
-        this.setListAdapter(myList);
+        // hier: verknüpfen der ListView mit der CheckedTextView
+        lv.setAdapter(myList);
+
+
+        // ImageView initialisieren
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        iv.setImageResource(R.drawable.hintergrund);
 
     }
 
@@ -65,6 +80,15 @@ public class MainActivity extends ListActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        lv.setItemChecked(position, true);
+
+        if (lv.isItemChecked(position)) {
+            btnAuswählen.setEnabled(true);
+        }
+    }
+    */
 
 }
 
